afb23e0a7a12fc5cabf31a5b35c6a8bfb17b9836
Shayana Shrestha <sshres25@vols.utk.edu>
2026-07-15 13:17:07 -0400

    Java initial test programs

---
+++ CodeSignal/Getting Started with Java/Unit 1/Multiply.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Multiply {
+    public static double multiplyInput(double n1, double n2, double n3) {
+        return n1 * n2 * n3;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter third number: ");
+        double num3 = scanner.nextDouble();
+
+        double result = multiplyInput(num1, num2, num3);
+        System.out.println("The product of numbers = " + result);
+
+        scanner.close();
+    }
+}
---
+++ CodeSignal/Getting Started with Java/Unit 1/solution.java
@@ -0,0 +1,5 @@
+class Solution{
+    public static void main(String[] args) {
+        System.out.println("Journey to the stars begins with Java!");
+    }
+}
