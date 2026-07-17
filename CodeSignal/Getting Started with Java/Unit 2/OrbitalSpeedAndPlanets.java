cb409e393c2cb47a1c4961132d5b7130e7ced9bd
Shayana Shrestha <shayana0324@gmail.com>
2026-07-17 18:52:32 -0400

    Orbital speed and number of planets

---
+++ CodeSignal/Getting Started with Java/Unit 2/OrbitalSpeedAndPlanets.java
@@ -0,0 +1,13 @@
+class OrbitalSpeedAndPlanets {
+        public static void main(String[] args) {
+            // Declare a constant for Earth's orbital speed and give it a value of 108000 (km/h)
+            final int ORBITAL_SPEED = 108000; // unit: km/h
+
+            // Declare a variable for the number of planets in the Solar System and assign it to 8
+            int numberOfPlanets = 8;        // Number of Planets
+
+            // Print out both the orbital speed of the Earth and the number of planets using print statements
+            System.out.println("Orbital speed of Earth = " + ORBITAL_SPEED );
+            System.out.println("The number of planets = " + numberOfPlanets);
+        }
+    }
