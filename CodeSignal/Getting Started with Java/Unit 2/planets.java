d1dfafa12b1b02f774311e4504e7f7f2e68c1798
Shayana Shrestha <shayana0324@gmail.com>
2026-07-17 18:25:10 -0400

    Unit 2 - planets

---
+++ CodeSignal/Getting Started with Java/Unit 2/planets.java
@@ -0,0 +1,9 @@
+class Planets {
+    public static void main(String[] args) {
+        final int SOLAR_SYSTEM_PLANETS = 8;
+        System.out.println("Number of planets in the Solar System: " + SOLAR_SYSTEM_PLANETS);
+
+        int universeAge = 14;
+        System.out.println("The universe is approximately " + universeAge + " billion years old.");
+    }
+}
