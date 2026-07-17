2b1978543a48b159e4ea3eb59b335629da5e463e
Shayana Shrestha <shayana0324@gmail.com>
2026-07-17 18:30:00 -0400

    Java constant

---
+++ CodeSignal/Getting Started with Java/Unit 2/Speed.java
@@ -0,0 +1,9 @@
+class Speed {
+    public static void main(String[] args) {
+        // Creating a constant for representing the speed of light
+        final int SPEED_OF_LIGHT_KM_S = 299792;
+
+        // Output the speed of light
+        System.out.println("The speed of light is: " + SPEED_OF_LIGHT_KM_S);
+    }
+}
