5d5c841d7ab2716c0d2dd63190bf5c8f52f94469
Shayana Shrestha <shayana0324@gmail.com>
2026-07-17 18:41:44 -0400

    Editing comment

--- CodeSignal/Getting Started with Java/Unit 2/OrbitalSpeed.java
+++ CodeSignal/Getting Started with Java/Unit 2/OrbitalSpeed.java
@@ -3,7 +3,7 @@ class OrbitalSpeed {
             // Declare a constant for Earth's orbital speed and set its value to 29 km/s
             final int ORBITAL_SPEED = 29;       // unit: km/s
             int planetsInSolarSystem = 8;       // Number of planets in the solar system
-            // TODO: Print out the string "Earth's orbital speed: <speed value> km/s
+            // Print out the string "Earth's orbital speed: <speed value> km/s"
             System.out.println("Number of planets: " + planetsInSolarSystem);
             System.out.println("Earth's orbital speed: " + ORBITAL_SPEED + " km/s");
         }
