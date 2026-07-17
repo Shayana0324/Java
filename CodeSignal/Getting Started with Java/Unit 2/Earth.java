c7a2b80a6f63d2d48ae5fa24ae2799c3346f9a12
Shayana Shrestha <shayana0324@gmail.com>
2026-07-17 18:33:13 -0400

    Planets

---
+++ CodeSignal/Getting Started with Java/Unit 2/Earth.java
@@ -0,0 +1,10 @@
+class Earth {
+   public static void main(String[] args) {
+            int earthOrbitRadius = 149600000; // in kilometers
+            System.out.println("The Earth's orbit radius is " + earthOrbitRadius + " kilometers.");
+
+            int planets = 8;
+            System.out.println("There are " + planets + " planets in the Solar System.");
+        }
+    }
+
