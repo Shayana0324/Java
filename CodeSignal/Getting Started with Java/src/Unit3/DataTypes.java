d615000086d36142eb428b90494efe7d2fe50154
Shayana Shrestha <shayana0324@gmail.com>
2026-07-20 14:56:15 -0400

    Unit 3 practice

---
+++ CodeSignal/Getting Started with Java/src/Unit3/DataTypes.java
@@ -0,0 +1,15 @@
+package Unit3;
+
+public class DataTypes {
+        public static void main(String[] args) {
+            float researchResult = 9.81f; // Gravitational constant in m/s^2
+            boolean isTheoryConfirmed = true; // The research confirms the theory
+            char grade = 'A'; // The quality assessment of the research
+            String conclusion = "Hypothesis verified.";
+
+            System.out.println(researchResult);
+            System.out.println("Is the theory confirmed? " + isTheoryConfirmed);
+            System.out.println("Research grade: " + grade);
+            System.out.println(conclusion);
+        }
+}
---
+++ CodeSignal/Getting Started with Java/src/Unit3/PrimitiveDataType.java
@@ -0,0 +1,24 @@
+package Unit3;
+
+public class PrimitiveDataType {
+        public static void main(String[] args) {
+            // Declare a float variable to store the distance from Earth to the Sun in kilometers         - 149600000.0 kilometers
+            float distance = 149600000.0f;          // Distance from earth to the Sun in km
+
+            // Declare an int variable to store the number of days Earth takes to orbit the Sun - 365 days
+            int numberOfDays = 365;                 // Number of days Earth takes to orbit the Sun
+
+            // Print the distance of Earth from the Sun followed by " km from the Sun."
+            System.out.println("Distance of Earth from the Sun " + distance + " km from the Sun.");
+
+            // Print the number of days Earth takes to orbit the Sun, followed by " days to orbit the Sun."
+            System.out.println("Number of days Earth takes to orbit the Sun " + numberOfDays + " days to orbit the Sun.");
+
+            // Declare a boolean variable to store the truth about whether space is dark - it is!
+            boolean space = true;
+
+            // Use a char variable to represent the first letter of 'Cosmos' - C
+            char cosmos = 'C';
+        }
+    }
+
