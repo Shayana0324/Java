0987ac39ea05e91a6649f82600841c86588c7408
Shayana Shrestha <shayana0324@gmail.com>
2026-07-22 13:50:37 -0400

    Double to string practice - unit 6

---
+++ CodeSignal/Getting Started with Java/src/Unit6/DoubleToString.java
@@ -0,0 +1,15 @@
+package Unit6;
+
+public class DoubleToString {
+    public static void main(String[] args) {
+        // A star's temperature in Kelvin
+
+        double starTemperature = 15000.95;
+
+        // Convert the temperature from a double to a String
+        String temperatureText = Double.toString(starTemperature); ;
+
+        // Displaying the temperature as text
+        System.out.println("Mission control, the star's temperature is: " + temperatureText + "K");
+    }
+}
