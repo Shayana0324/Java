027ea9d0f51d540ba52957eca34c754b0045791e
Shayana Shrestha <shayana0324@gmail.com>
2026-07-27 13:12:28 -0400

    Adding comment in LastInListInt

--- Beginner-to-Advanced-Java/src/ListPractice/LastInListInt.java
+++ Beginner-to-Advanced-Java/src/ListPractice/LastInListInt.java
@@ -1,3 +1,12 @@
+/*
+* The exercise template contains a base that reads numbers from the user
+* and adds them to a list. Reading is stopped once the user
+* enters the number -1.
+
+* Expand the functionality of the program so that after reading the numbers,
+* it prints all the numbers received from the user.
+* The number used to indicate stopping should not be printed.
+* */
 package src.ListPractice;
 
 import java.util.ArrayList;
