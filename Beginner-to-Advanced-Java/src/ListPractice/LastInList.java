c7a71aa0859d7005135c1d717eb5c43bba830e6b
Shayana Shrestha <shayana0324@gmail.com>
2026-07-27 12:57:01 -0400

    Printing last in list practice

---
+++ Beginner-to-Advanced-Java/src/ListPractice/LastInList.java
@@ -0,0 +1,27 @@
+package src.ListPractice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastInList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Enter a name:");
+            String userInput = scanner.nextLine();
+
+            if (userInput.trim().isEmpty()){
+                System.out.println("Last name on the list: " + names.get(names.toArray().length - 1));
+                break;
+            }
+            names.add(userInput);
+        }
+        System.out.println("Names in the list:" + names);
+
+
+
+    }
+}
