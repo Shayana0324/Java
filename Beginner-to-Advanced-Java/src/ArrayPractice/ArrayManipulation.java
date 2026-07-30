1083d60c73b91c970a927e37672a41c63d5d8c8d
Shayana Shrestha <shayana0324@gmail.com>
2026-07-30 13:26:04 -0400

    Array manipulation

--- Beginner-to-Advanced-Java/src/ArrayPractice/ArrayManipulation.java
+++ Beginner-to-Advanced-Java/src/ArrayPractice/ArrayManipulation.java
@@ -11,6 +11,13 @@ public class ArrayManipulation {
     public static void main(String[] args) {
         int[] numbers = {5, 1, 3,4, 2};
         System.out.println("Sum of integers in array:" + sumOfNumbersInArray(numbers));
+
+        String[] arrayOfStrings = {"Matti L.", "Matti P.", "Matti V."};
+        double[] arrayOfFloatingpoints = {1.20, 3.14, 100.0, 0.66666667};
+
+        for(int i = 0; i < arrayOfFloatingpoints.length; i++) {
+            System.out.println(arrayOfStrings[i] + " " + arrayOfFloatingpoints[i]);
+        }
     }
 
 }
