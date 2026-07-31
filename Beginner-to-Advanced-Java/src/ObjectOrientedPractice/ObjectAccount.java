c21ace5b9d04fd052ca58416686a00f2f9927ee6
Shayana Shrestha <shayana0324@gmail.com>
2026-07-31 14:48:02 -0400

    Adding comments

--- Beginner-to-Advanced-Java/src/ObjectOrientedPractice/ObjectAccount.java
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/ObjectAccount.java
@@ -19,6 +19,14 @@ public class ObjectAccount {
         System.out.println(artosAccount);
         System.out.println(artosSwissAccount);
 
+        /*
+        * Write a program that:
+        * Creates an account named "Matthews account"with the balance 1000
+        * Creates an account named "My account"with the balance 0
+        * Withdraws 100.0 from Matthew's account
+        * Deposits 100.0 to "my account"
+        * Prints both accounts
+        * */
         Account matthewsAccount = new Account("Matthew's Account", 1000.00);
         Account myAccount = new Account("My Acount", 0.00);
 
