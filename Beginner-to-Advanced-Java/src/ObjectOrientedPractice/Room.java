6ccbd2472dad6a4156bfcbef90c6e89915a367b2
Shayana Shrestha <shayana0324@gmail.com>
2026-08-01 10:41:43 -0400

    Practicing constructor

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Room.java
@@ -0,0 +1,11 @@
+package src.ObjectOrientedPractice;
+
+public class Room {
+    private String code;
+    private int seats;
+
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+}
