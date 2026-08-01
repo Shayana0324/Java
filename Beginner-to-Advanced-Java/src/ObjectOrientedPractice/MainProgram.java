d226f681a9092c90d648a980db5e662dcd5f9062
Shayana Shrestha <shayana0324@gmail.com>
2026-08-01 14:06:19 -0400

    Decreasing Counter

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/MainProgram.java
@@ -0,0 +1,19 @@
+package src.ObjectOrientedPractice;
+import src.ObjectOrientedPractice.DecreasingCounter;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+        counter.reset();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+    }
+}
