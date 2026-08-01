9bcd5b55473528059e29a7b2c7bed899695f43d5
Shayana Shrestha <shayana0324@gmail.com>
2026-08-01 12:06:16 -0400

    changing an instance variable's value in a method

--- Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Person.java
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Person.java
@@ -14,10 +14,25 @@ public class Person {
         System.out.println(this.name + ", age " + this.age + " years");
     }
 
+    // Changing an Instance Variable's value in a method
+    // growOlder() method added
+    public void growOlder() {
+        this.age = this.age + 1;
+    }
+
     public static void main(String[] args) {
         Person ada = new Person("Ada");
         Person antti = new Person("Antti");
         Person martin = new Person("Martin");
+
+        ada.printPerson();
+        antti.printPerson();
+        martin.printPerson();
+        System.out.println("");
+
+        ada.growOlder();
+        ada.growOlder();
+        antti.growOlder();
 
         ada.printPerson();
         antti.printPerson();
