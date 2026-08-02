48abef655007c5dfddeab35424aa3ea870be3cc8
Shayana Shrestha <shayana0324@gmail.com>
2026-08-02 13:09:13 -0400

    Adding comments in gauge class

--- Beginner-to-Advanced-Java/src/ObjectOrientedPractice/GaugeCheck/Gauge.java
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/GaugeCheck/Gauge.java
@@ -7,22 +7,43 @@ public class Gauge {
        int initialMeterValue = 0;
     }
 
+    /*
+    * Method public void increase()
+    * grows the value instance
+    * variable's value by one. It does not grow the value
+    * beyond five.
+    * */
     public void increase() {
         if(this.value <= 5) {
             this.value = this.value + 1;
         }
     }
 
+    /*
+    * Method public void decrease()
+    * decreases the value instance variable's value by
+    * one. It does not decrease the value to
+    * negative numbers.
+    * */
     public void decrease() {
         if(this.value >=0) {
             this.value = this.value - 1;
         }
     }
 
+    /*
+    * Method public int value()
+    * Returns the value variable's value.
+    * */
     public int value() {
         return this.value;
     }
 
+    /*
+    * Method public boolean full()
+    * Returns true if the instance variable value has the
+    * value five. Otherwise, it Returns false.
+    * */
     public boolean full() {
         if(this.value == 5) {
             return true;
