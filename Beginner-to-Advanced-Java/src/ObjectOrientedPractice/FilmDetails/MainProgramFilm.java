06e17ce515fd778689f1ca8a9a731017a090b448
Shayana Shrestha <shayana0324@gmail.com>
2026-08-01 23:16:20 -0400

    Film Class Practice

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/FilmDetails/MainProgramFilm.java
@@ -0,0 +1,22 @@
+package src.ObjectOrientedPractice.FilmDetails;
+import java.util.Scanner;
+import src.ObjectOrientedPractice.FilmDetails.Film;
+
+public class MainProgramFilm {
+    public static void main(String[] args) {
+        Film chipmunks = new Film("Alvin and the Chipmunks: The Squeakquel", 0);
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(reader.nextLine());
+
+        System.out.println();
+        if (age >= chipmunks.ageRating()) {
+            System.out.println("You may watch the film " + chipmunks.name());
+        } else {
+            System.out.println("You may not watch the film " + chipmunks.name());
+        }
+    }
+
+}
