e50a1782f36935c6e158e9aa3b15a64194eac5d6
Shayana Shrestha <shayana0324@gmail.com>
2026-08-02 12:50:40 -0400

    Main program gauge

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/GaugeCheck/MainProgramGauge.java
@@ -0,0 +1,17 @@
+package src.ObjectOrientedPractice.GaugeCheck;
+import src.ObjectOrientedPractice.GaugeCheck.Gauge;
+
+public class MainProgramGauge {
+    public static void main(String[] args) {
+        Gauge g = new Gauge();
+
+        while(!g.full()) {
+            System.out.println("Not full! Value: " + g.value());
+            g.increase();
+        }
+
+        System.out.println("Full! Value: " + g.value());
+        g.decrease();
+        System.out.println("Not full! Value: " + g.value());
+    }
+}
