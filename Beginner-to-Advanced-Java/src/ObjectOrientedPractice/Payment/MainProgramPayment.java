6ed3eb09c5d69e8eb6e021399934b48ac4a8923b
Shayana Shrestha <shayana0324@gmail.com>
2026-08-03 09:13:41 -0400

    Main program payment card

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Payment/MainProgramPayment.java
@@ -0,0 +1,9 @@
+package src.ObjectOrientedPractice.Payment;
+import src.ObjectOrientedPractice.Payment.PaymentCard;
+
+public class MainProgramPayment {
+    public static void main(String[] args) {
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
+    }
+}
