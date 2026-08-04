129add087732c9458d193de56d0648c9710559ec
Shayana Shrestha <shayana0324@gmail.com>
2026-08-04 10:01:23 -0400

    Adding user-inputted objects to a list

--- Beginner-to-Advanced-Java/src/ObjectsInAList/MainProgramPerson.java
+++ Beginner-to-Advanced-Java/src/ObjectsInAList/MainProgramPerson.java
@@ -1,18 +1,28 @@
 package src.ObjectsInAList;
 import src.ObjectsInAList.Person;
 import java.util.ArrayList;
-
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainProgramPerson {
     public static void main() {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Person> persons = new ArrayList<>();
 
-        Person john = new Person("John");
-        persons.add(john);
+        // Read the names of persons from the user
+        while(true) {
+            System.out.println("Enter a name, empty will stop: ");
+            String name = scanner.nextLine();
+            if(name.isEmpty()) {
+                break;
+            }
 
-        persons.add(new Person("Matthew"));
-        persons.add(new Person("Martin"));
+            // Add to the list a new person whose name is the previous user input
+            persons.add(new Person(name));
+        }
+
+        System.out.println();
+        System.out.println("Persons in total: " + persons.size());
+        System.out.println("Persons: " );
 
         for (Person person: persons) {
             System.out.println(person);
