d6ef359f99f9471d08df04a6eb25520b137d6fd6
Shayana Shrestha <shayana0324@gmail.com>
2026-08-05 14:09:27 -0400

    Main Program Health Station

--- Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Health/MainProgramHealthStation.java
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Health/MainProgramHealthStation.java
@@ -1,4 +1,16 @@
 package src.ObjectOrientedPractice.Health;
+import src.ObjectOrientedPractice.Health.HealthStation;
+import src.ObjectOrientedPractice.Person;
 
 public class MainProgramHealthStation {
+    public static void main(String[] args) {
+        HealthStation childrenHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println(ethan.getName() + " weight: " + childrenHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrenHospital.weigh(peter) + " kilos");
+
+    }
 }
