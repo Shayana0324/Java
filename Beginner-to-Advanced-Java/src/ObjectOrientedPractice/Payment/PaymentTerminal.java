1601cc4f190e7a0cf75ecb92bd020a26b2cfb41f
Shayana Shrestha <shayana0324@gmail.com>
2026-08-06 11:34:28 -0400

    payment terminal class

---
+++ Beginner-to-Advanced-Java/src/ObjectOrientedPractice/Payment/PaymentTerminal.java
@@ -0,0 +1,42 @@
+package src.ObjectOrientedPractice.Payment;
+
+public class PaymentTerminal {
+    private double money;           // Amount of cash
+    private int affordableMeals;        // number of sold affordable meals
+    private int heartyMeals;        // number of sold hearty meals
+
+    public PaymentTerminal() {
+        // register initially has 1000 euros of money
+        this.money = 1000.0;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
+    }
+
+    public double eatAffordably(double payment) {
+        // an affordable meal costs 2.50 euros
+        // increase the amount of cash by the price of an affordable meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        if(payment >= 2.50) {
+            this.money += 2.50;
+            this.affordableMeals++;
+            return payment - 2.50;
+        }
+        return payment;
+    }
+
+    public double eatHeartily(double payment) {
+        // a hearty meal costs 4.30 euros
+        // increase the amount of cash by the price of a hearty meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        if(payment >= 4.30) {
+            this.money += 4.30;
+            this.heartyMeals++;
+            return payment - 4.30;
+        }
+        return payment;
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
