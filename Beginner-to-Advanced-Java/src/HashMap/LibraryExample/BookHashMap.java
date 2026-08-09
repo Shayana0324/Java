f6297c0c99ff9698f5bd92b8ddb85b39bc5c5acf
Shayana Shrestha <shayana0324@gmail.com>
2026-08-09 19:08:05 -0400

    Book hashmap

---
+++ Beginner-to-Advanced-Java/src/HashMap/LibraryExample/BookHashMap.java
@@ -0,0 +1,24 @@
+package src.HashMap.LibraryExample;
+
+import java.util.HashMap;
+
+public class BookHashMap {
+    public static void main(String[] args) {
+
+        Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
+        Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "...");
+
+        HashMap<String, Book> directory = new HashMap<>();
+
+        directory.put(senseAndSensibility.getName(), senseAndSensibility);
+        directory.put(prideAndPrejudice.getName(), prideAndPrejudice);
+
+        Book book = directory.get("Persuasion");
+        System.out.println(book);
+        System.out.println();
+        book = directory.get("Pride and Prejudice");
+        System.out.println(book);
+
+
+    }
+}
