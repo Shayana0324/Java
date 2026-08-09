cf070f322f9e956bf8ed597a2b465dee7b0ea4c9
Shayana Shrestha <shayana0324@gmail.com>
2026-08-09 14:48:44 -0400

    Postal codes practice

---
+++ Beginner-to-Advanced-Java/src/HashMap/Practice/PostalCode.java
@@ -0,0 +1,16 @@
+package src.HashMap.Practice;
+
+import java.util.HashMap;
+
+public class PostalCode {
+    public static void main(String[] args) {
+        HashMap<String, String> postalCodes = new HashMap<>();
+
+        postalCodes.put("00710", "Helsinki");
+        postalCodes.put("90014", "Oulu");
+        postalCodes.put("33720", "Tampere");
+        postalCodes.put("33014", "Tampere");
+
+        System.out.println(postalCodes.get("00710"));
+    }
+}
