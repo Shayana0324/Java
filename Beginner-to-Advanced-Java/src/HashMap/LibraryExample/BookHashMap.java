653ed16586999f55adc73334251c6917ce7c9f68
Shayana Shrestha <shayana0324@gmail.com>
2026-08-09 19:30:44 -0400

    adding get method

--- Beginner-to-Advanced-Java/src/HashMap/LibraryExample/BookHashMap.java
+++ Beginner-to-Advanced-Java/src/HashMap/LibraryExample/BookHashMap.java
@@ -1,24 +1,42 @@
 package src.HashMap.LibraryExample;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BookHashMap {
+    public static Book get(ArrayList<Book> books, String name) {
+        for(Book book: books) {
+            if(book.getName().equals(name)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
 
         Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
         Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "...");
+        ArrayList<Book> books = new ArrayList<>();
+
+        books.add(senseAndSensibility);
+        books.add(prideAndPrejudice);
 
         HashMap<String, Book> directory = new HashMap<>();
 
         directory.put(senseAndSensibility.getName(), senseAndSensibility);
         directory.put(prideAndPrejudice.getName(), prideAndPrejudice);
 
-        Book book = directory.get("Persuasion");
-        System.out.println(book);
-        System.out.println();
-        book = directory.get("Pride and Prejudice");
-        System.out.println(book);
+//        Book book = directory.get("Persuasion");
+//        System.out.println();
+//        System.out.println(book);
+//        book = directory.get("Pride and Prejudice");
+//        System.out.println(book);
 
+        // searching for a book name Sense and Sensibility
+        System.out.println(get(books, "Sense and Sensibility"));
+        System.out.println();
+        System.out.println(get(books, "Persuasion"));
 
     }
 }
